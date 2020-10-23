3bbbe75bba1c498adea7a31a40c9197b2c3e7651
Sunny Jha <sunnyjha11897@gmail.com>
2020-10-23 21:47:04 +0530

    [SUNNY]Add.UseCase1 to create binary search tree

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -7,6 +7,11 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Binary Search Tree Implementation!");
+            Binary<int> binarySearch = new Binary<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
+            binarySearch.GetSize();
         }
     }
 }
